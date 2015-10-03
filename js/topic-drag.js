f32492b76b9b45c163e06b3efe6ee85887349319
orzzyd <zhengyunda@hotmail.com>
2015-10-03 21:16:24 +0800

    Avoid to pollute global variable and remove some pictures.

--- js/topic-drag.js
+++ js/topic-drag.js
@@ -1,67 +1,71 @@
-$(document).ready(function() {
-	bindItemMousedownEvent();
-	bindItemMouseupEvent();
-});
-function bindItemMousedownEvent(){
-	$(".always-topics-items").mousedown(function(event) {
-		/* Act on the event */
-		event.preventDefault();
-		$(this).addClass('draging');
-		var $itemOffset=$(this).offset();
-		var $mouseOffsetX=event.pageX-$itemOffset.left;
-		var $mouseOffsetY=event.pageY-$itemOffset.top;
-		var $pageX=event.pageX;
-		var $pageY=event.pageY;
+(function () {
+	$(document).ready(function () {
+		bindItemMousedownEvent();
+		bindItemMouseupEvent();
+	});
 
-		$dragingItem=$(this);
-		$cloneItem=$(this).clone(true).insertAfter($(this)).css('visibility', 'hidden').hide();
-		
-		$(document).mousemove(function(event) {
+	var $dragingItem = null;
+	var $cloneItem = null;
+	var $tempIndex = -1;
+
+	function bindItemMousedownEvent() {
+		$(".always-topics-items").mousedown(function (event) {
 			/* Act on the event */
 			event.preventDefault();
-			$dragingItem.appendTo($dragingItem.parent());
-			$dragingItem.css({
-				position: 'fixed',
-				left: event.pageX-$mouseOffsetX-$(document).scrollLeft(),
-				top: event.pageY-$mouseOffsetY-$(document).scrollTop(),
+			$(this).addClass('draging');
+			var $itemOffset = $(this).offset();
+			var $mouseOffsetX = event.pageX - $itemOffset.left;
+			var $mouseOffsetY = event.pageY - $itemOffset.top;
+			var $pageX = event.pageX;
+			var $pageY = event.pageY;
+
+			$dragingItem = $(this);
+			$cloneItem = $(this).clone(true).insertAfter($(this)).css('visibility', 'hidden').hide();
+
+			$(document).mousemove(function (event) {
+				/* Act on the event */
+				event.preventDefault();
+				$dragingItem.appendTo($dragingItem.parent());
+				$dragingItem.css({
+					position: 'fixed',
+					left: event.pageX - $mouseOffsetX - $(document).scrollLeft(),
+					top: event.pageY - $mouseOffsetY - $(document).scrollTop(),
+				});
+
+				if ((event.pageX - $pageX <= 135) && (event.pageX - $pageX >= -135) && ($dragingItem.offset().top >= 250) && ($dragingItem.offset().top <= 500)) {
+					$cloneItem.show();
+					if (($dragingItem.offset().top >= 250) && ($dragingItem.offset().top < 330)) {
+						$tempIndex = 0;
+					};
+					if (($dragingItem.offset().top >= 330) && ($dragingItem.offset().top < 400)) {
+						$tempIndex = 1;
+					};
+					if (($dragingItem.offset().top >= 400) && ($dragingItem.offset().top <= 500)) {
+						$tempIndex = 2;
+					};
+
+					$cloneItem.appendTo($cloneItem.parent());
+					$cloneItem.insertBefore($cloneItem.parent().children().eq($tempIndex));
+				} else {
+					$cloneItem.hide();
+				}
 			});
 
-			if((event.pageX-$pageX<=135)&&(event.pageX-$pageX>=-135)&&($dragingItem.offset().top>=250)&&($dragingItem.offset().top<=500)){
-				$cloneItem.show();
-				if (($dragingItem.offset().top>=250)&&($dragingItem.offset().top<330)) {
-					$tempIndex=0;
-				};
-				if (($dragingItem.offset().top>=330)&&($dragingItem.offset().top<400)) {
-					$tempIndex=1;
-				};
-				if (($dragingItem.offset().top>=400)&&($dragingItem.offset().top<=500)) {
-					$tempIndex=2;
-				};
-
-				$cloneItem.appendTo($cloneItem.parent());
-				$cloneItem.insertBefore($cloneItem.parent().children().eq($tempIndex));
-			}else{
-				$cloneItem.hide();
-			}
 		});
+	}
+	function bindItemMouseupEvent() {
+		$(document).mouseup(function (event) {
+			/* Act on the event */
+			event.preventDefault();
+			$(".always-topics-items").removeClass('draging');
+			$(document).unbind('mousemove');
 
-	});
-}
-function bindItemMouseupEvent(){
-	$(document).mouseup(function(event) {
-		/* Act on the event */
-		event.preventDefault();
-		$(".always-topics-items").removeClass('draging');
-		$(document).unbind('mousemove');
+			if ($cloneItem) {
+				$cloneItem.css('visibility', 'visible').show();
+			};
 
-		if ($cloneItem) {
-			$cloneItem.css('visibility', 'visible').show();
-		};
-		
-		$dragingItem.remove();
-	});
-}
+			$dragingItem.remove();
+		});
+	}
 
-var $dragingItem=null;
-var $cloneItem=null;
-var $tempIndex=-1;
+})();
